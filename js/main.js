7a7d6ca114cd5acf11fefc609f2e4b9e0164f6f4
Donchelox <inside.4.me@gmail.com>
2021-04-06 19:44:28 -0400

    funcionalidad 360

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 var scrollMenu = document.getElementById("scroll-menu");
-
+var aSky = document.querySelector('a-sky');
 
 function toggleSiderbarMenu(){
     document.getElementById("sidebar-menu").classList.toggle('cerrar');
@@ -16,4 +16,25 @@ function regresarMenu(){
 function sedesCTEC(){
     scrollMenu.removeAttribute('class');
     scrollMenu.classList.toggle('administracion-active');
+}
+
+function volver360(){
+    document.getElementById("galeria-miniaturas").classList.toggle('d-none');
+    document.getElementById("recorrido-360").classList.toggle('d-none');
+}
+
+function pMontt1(){
+    document.getElementById("galeria-miniaturas").classList.toggle('d-none');
+    document.getElementById("recorrido-360").classList.toggle('d-none');
+    aSky.setAttribute('src', '../../../img/sedes/ct-puertomontt/360/R0010756.jpg');
+}
+function pMontt2(){
+    document.getElementById("galeria-miniaturas").classList.toggle('d-none');
+    document.getElementById("recorrido-360").classList.toggle('d-none');
+    aSky.setAttribute('src', '../../../img/sedes/ct-puertomontt/360/R0010756.jpg');
+}
+function pMontt3(){
+    document.getElementById("galeria-miniaturas").classList.toggle('d-none');
+    document.getElementById("recorrido-360").classList.toggle('d-none');
+    aSky.setAttribute('src', 'https://donchelox.github.io/ipchile-360/img/escuelas/administracion/taller-aplicaciones-diseno-2dy3d/republica/360/diseno-republica01.jpg');
 }
