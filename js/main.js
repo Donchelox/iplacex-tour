4ae5fb0d32490bfe4504f7cc275e5da9ef5ffd85
Donchelox <inside.4.me@gmail.com>
2021-04-08 21:53:14 -0400

    orden carpetas

--- js/main.js
+++ js/main.js
@@ -25,6 +25,109 @@ function volver360(){
     document.getElementById("recorrido-360").classList.toggle('d-none');
 }
 
+// 360 sede santiago
+function sedeSantiago1(){
+    document.getElementById("galeria-miniaturas").classList.toggle('d-none');
+    document.getElementById("recorrido-360").classList.toggle('d-none');
+    aSky.setAttribute('src', '../../../img/sedes/ct-puertomontt/360/R0010756.jpg');
+}
+function sedeSantiago2(){
+    document.getElementById("galeria-miniaturas").classList.toggle('d-none');
+    document.getElementById("recorrido-360").classList.toggle('d-none');
+    aSky.setAttribute('src', '../../../img/sedes/ct-puertomontt/360/R0010756.jpg');
+}
+function sedeSantiago3(){
+    document.getElementById("galeria-miniaturas").classList.toggle('d-none');
+    document.getElementById("recorrido-360").classList.toggle('d-none');
+    aSky.setAttribute('src', 'https://donchelox.github.io/ipchile-360/img/escuelas/administracion/taller-aplicaciones-diseno-2dy3d/republica/360/diseno-republica01.jpg');
+}
+
+// 360 sede Copiapo
+function sedeCopiapo1(){
+    document.getElementById("galeria-miniaturas").classList.toggle('d-none');
+    document.getElementById("recorrido-360").classList.toggle('d-none');
+    aSky.setAttribute('src', '../../../img/sedes/ct-puertomontt/360/R0010756.jpg');
+}
+function sedeCopiapo2(){
+    document.getElementById("galeria-miniaturas").classList.toggle('d-none');
+    document.getElementById("recorrido-360").classList.toggle('d-none');
+    aSky.setAttribute('src', '../../../img/sedes/ct-puertomontt/360/R0010756.jpg');
+}
+function sedeCopiapo3(){
+    document.getElementById("galeria-miniaturas").classList.toggle('d-none');
+    document.getElementById("recorrido-360").classList.toggle('d-none');
+    aSky.setAttribute('src', 'https://donchelox.github.io/ipchile-360/img/escuelas/administracion/taller-aplicaciones-diseno-2dy3d/republica/360/diseno-republica01.jpg');
+}
+
+// 360 sede Talca
+function sedeTalca1(){
+    document.getElementById("galeria-miniaturas").classList.toggle('d-none');
+    document.getElementById("recorrido-360").classList.toggle('d-none');
+    aSky.setAttribute('src', '../../../img/sedes/ct-puertomontt/360/R0010756.jpg');
+}
+function sedeTalca2(){
+    document.getElementById("galeria-miniaturas").classList.toggle('d-none');
+    document.getElementById("recorrido-360").classList.toggle('d-none');
+    aSky.setAttribute('src', '../../../img/sedes/ct-puertomontt/360/R0010756.jpg');
+}
+function sedeTalca3(){
+    document.getElementById("galeria-miniaturas").classList.toggle('d-none');
+    document.getElementById("recorrido-360").classList.toggle('d-none');
+    aSky.setAttribute('src', 'https://donchelox.github.io/ipchile-360/img/escuelas/administracion/taller-aplicaciones-diseno-2dy3d/republica/360/diseno-republica01.jpg');
+}
+
+// 360 Ctec Arica
+function ctecArica1(){
+    document.getElementById("galeria-miniaturas").classList.toggle('d-none');
+    document.getElementById("recorrido-360").classList.toggle('d-none');
+    aSky.setAttribute('src', '../../../img/sedes/ct-puertomontt/360/R0010756.jpg');
+}
+function ctecArica2(){
+    document.getElementById("galeria-miniaturas").classList.toggle('d-none');
+    document.getElementById("recorrido-360").classList.toggle('d-none');
+    aSky.setAttribute('src', '../../../img/sedes/ct-puertomontt/360/R0010756.jpg');
+}
+function ctecArica3(){
+    document.getElementById("galeria-miniaturas").classList.toggle('d-none');
+    document.getElementById("recorrido-360").classList.toggle('d-none');
+    aSky.setAttribute('src', 'https://donchelox.github.io/ipchile-360/img/escuelas/administracion/taller-aplicaciones-diseno-2dy3d/republica/360/diseno-republica01.jpg');
+}
+
+// 360 Ctec Antofagasta
+function ctecAntofagasta1(){
+    document.getElementById("galeria-miniaturas").classList.toggle('d-none');
+    document.getElementById("recorrido-360").classList.toggle('d-none');
+    aSky.setAttribute('src', '../../../img/sedes/ct-puertomontt/360/R0010756.jpg');
+}
+function ctecAntofagasta2(){
+    document.getElementById("galeria-miniaturas").classList.toggle('d-none');
+    document.getElementById("recorrido-360").classList.toggle('d-none');
+    aSky.setAttribute('src', '../../../img/sedes/ct-puertomontt/360/R0010756.jpg');
+}
+function ctecAntofagasta3(){
+    document.getElementById("galeria-miniaturas").classList.toggle('d-none');
+    document.getElementById("recorrido-360").classList.toggle('d-none');
+    aSky.setAttribute('src', 'https://donchelox.github.io/ipchile-360/img/escuelas/administracion/taller-aplicaciones-diseno-2dy3d/republica/360/diseno-republica01.jpg');
+}
+
+// 360 Ctec Arica
+function ctecArica1(){
+    document.getElementById("galeria-miniaturas").classList.toggle('d-none');
+    document.getElementById("recorrido-360").classList.toggle('d-none');
+    aSky.setAttribute('src', '../../../img/sedes/ct-puertomontt/360/R0010756.jpg');
+}
+function ctecArica2(){
+    document.getElementById("galeria-miniaturas").classList.toggle('d-none');
+    document.getElementById("recorrido-360").classList.toggle('d-none');
+    aSky.setAttribute('src', '../../../img/sedes/ct-puertomontt/360/R0010756.jpg');
+}
+function ctecArica3(){
+    document.getElementById("galeria-miniaturas").classList.toggle('d-none');
+    document.getElementById("recorrido-360").classList.toggle('d-none');
+    aSky.setAttribute('src', 'https://donchelox.github.io/ipchile-360/img/escuelas/administracion/taller-aplicaciones-diseno-2dy3d/republica/360/diseno-republica01.jpg');
+}
+
+// 360 Ctec puerto montt
 function pMontt1(){
     document.getElementById("galeria-miniaturas").classList.toggle('d-none');
     document.getElementById("recorrido-360").classList.toggle('d-none');
